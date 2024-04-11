3f8dcb7a9bf413dda478f2db4bddef2d635db1d8
Xsandex <165285832+Xsandex@users.noreply.github.com>
2024-04-11 19:23:18 +0500

    Update CountryPong.py

--- CountryPong.py
+++ CountryPong.py
@@ -3,22 +3,71 @@ from pygame import *
 
 window = display.set_mode((500,500))
 
+background = transform.scale(image.load(r"C:\Users\STAS\OneDrive\Рабочий стол\CountryPong-main\sad.jpg"),(500,500))
+
+
+events = event.get()
+events[0].type
+clock = time.Clock()
+germany = transform.scale(image.load(r'C:\Users\STAS\OneDrive\Рабочий стол\CountryPong-main\Gremany.png'),(10,200))
 game = True
-pressed_key = pygame.key.get_pressed()
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
 
-class player_1():
-    def __init__(self,player_speed,player_size)
-        self.speed = player_speed
-        self.size = player_size
+
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+
+
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def move():
+        if sprite.collide_rect(player_1,Ball):
+            self.rect.x -= self.speed
+            self.rect.y -= self.speed
+        if sprite.collide_rect(player_2,Ball):
+            self.rect.x += self.speed
+            self.rect.y += self.speed
+class player_1(GameSprite):
+    
     def control():
-        if pressed_key
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if keys_pressed[K_s] and self.rect.y < 415:
+            self.rect.y += self.speed
+class player_2(GameSprite):
+    
+    def control():
+        keys_pressed = key.get_pressed()
+        if keys_pressed[KEY_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if keys_pressed[KEY_DOWN] and self.rect.y < 415:
+            self.rect.y += self.speed
+    
 
+Germany = player_1(r'C:\Users\STAS\OneDrive\Рабочий стол\CountryPong-main\Gremany.png',25,250,10,100,5)
+USSR = player_2(r'C:\Users\STAS\OneDrive\Рабочий стол\CountryPong-main\USSR.png',470,250,10,100,5)
+Poland = Ball(r'C:\Users\STAS\OneDrive\Рабочий стол\CountryPong-main\POLAND.png',250,250,10,10,5)
 
 while game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    window.blit(background,(0,0))
+
+    for e in event.get():
+        if e.type == QUIT:
             game = False
-
-
+    window.blit(germany,(10,100))
+    display.update()
 clock.tick(60)
-pygame.display.update()
+     
